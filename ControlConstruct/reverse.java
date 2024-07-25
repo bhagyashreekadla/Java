5ba2deed1f9ef5e4e17e75f14f8d2478154d9854
bhagyashreekadla <144587137+bhagyashreekadla@users.noreply.github.com>
2024-07-26 00:09:53 +0530

    Update reverse.java

--- ControlConstruct/reverse.java
+++ ControlConstruct/reverse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Reversenumber {
+public class reverse {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a number: ");
