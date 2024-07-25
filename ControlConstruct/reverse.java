72521239d34bdcf7447afdff338bef41e36a3d73
bhagyashreekadla <144587137+bhagyashreekadla@users.noreply.github.com>
2024-07-25 23:53:22 +0530

    Create reverse.java

---
+++ ControlConstruct/reverse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Reversenumber {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = scanner.nextInt();
+		int reversedNumber = reverse(number);
+		System.out.println( reversedNumber);
+       }
+	public static int reverse(int num) {
+		int reversed = 0;
+		while (num != 0) {
+			int digit = num % 10;
+			reversed = reversed * 10 + digit;
+			num /= 10;
+	  }
+		return reversed;
+	}
+}
+
+
